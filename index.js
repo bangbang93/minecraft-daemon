f92b51da4bf1f69544f79599c06e29a84e0c3eb6
bangbang93 <bangbang93@163.com>
2014-11-03 17:50:08 +0800

    继承EventEmitter

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
  * Created by bangbang93 on 14-11-3.
  */
 var launcher = require('./launcher');
+var events = require('events');
+var util = require('util');
 
 var servers = {};
 
-module.exports = function (java){
+var daemon = function (java){
     var that = this;
     var Launcher = launcher(java);
     this.addServer = function (serverName, serverPath, jarFile, opt){
@@ -16,12 +18,24 @@ module.exports = function (java){
         return servers[serverName];
     };
     this.deleteServer = function (serverName){
+        var server = this.getServer(serverName);
+        if (server && server.isRunning){
+            return false;
+        }
         delete servers[serverName];
+        return true;
     };
     this.startServer = function (serverName){
+        var that = this;
         var server = this.getServer(serverName);
         if (!!server){
             if (server.start()){
+                server.on('output', function (data){
+                    that.emit('output', {
+                        server: serverName,
+                        data: data
+                    })
+                });
                 return {
                     stdin: server.stdin,
                     stdout: server.stdout,
@@ -47,5 +61,13 @@ module.exports = function (java){
                 server.kill();
             }
         }
-    }
+    };
+    this.changeJava = function (javaPath){
+        java = javaPath;
+    };
+
 };
+
+util.inherits(daemon, events.EventEmitter);
+
+module.exports = daemon;
